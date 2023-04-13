33a79c01ebf10d8984e0eec33b52971d1c182805
tsagueu brandel <tsagueubrandel@gmail.com>
2023-04-13 16:54:15 +0200

    add Home screen and use of SharedPreferences to get user setted color mode between two sessions

--- lib/screens/expenses_page_tabs/ExpensesYearTab.dart
+++ lib/screens/expenses_page_tabs/ExpensesYearTab.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:expenses_app_2/screens/transaction_detail.dart';
 import 'package:expenses_app_2/store/transaction_provider.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +6,9 @@ import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../models/transaction.dart';
+
+// custom theme
+import '../../themes/main.dart' as main;
 
 class ExpensesYearTab extends StatefulWidget {
   const ExpensesYearTab({Key? key}) : super(key: key);
@@ -66,77 +67,23 @@ class _ExpensesYearTabState extends State<ExpensesYearTab> {
                         ],
                       ),
                     ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.background,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Column(
-                          children: yearTransactions.map(
+                    Column(
+                      children: yearTransactions.map(
                             (transaction) {
-                              return Padding(
-                                padding: const EdgeInsets.only(bottom: 6),
-                                child: ListTile(
-                                  leading: CircleAvatar(
-                                    backgroundColor: Colors.transparent,
-                                    child: SizedBox(
-                                      child: Image.file(
-                                          File(transaction["imagePath"])),
-                                    ),
-                                  ),
-                                  title: Text(
-                                    transaction["name"],
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  subtitle: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(transaction["reason"]),
-                                      Text(
-                                        DateFormat.yMEd(
-                                                appProvider.locale.countryCode)
-                                            .format(DateTime.parse(
-                                                transaction["date"])),
-                                        style: TextStyle(
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .tertiary,
-                                            fontSize: 11),
-                                      ),
-                                    ],
-                                  ),
-                                  trailing: Text(
-                                    "${transaction["amount"]} €",
-                                    style:
-                                        Theme.of(context).textTheme.titleMedium,
-                                  ),
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                    context,
-                                    TransactionDetail.routeName,
-                                    arguments: Transaction(
-                                      id: transaction['id'],
-                                      name: transaction['name'],
-                                      reason: transaction['reason'],
-                                      amount: transaction['amount'],
-                                      imagePath: transaction['imagePath'],
-                                      date: transaction['date'],
-                                    ),
-                                  );
-                                  },
-                                ),
-                              );
-                            },
-                          ).toList(),
-                        ),
-                      ),
+                          return _transactionItem(
+                              context,
+                              provider: appProvider,
+                              transaction: Transaction(
+                                  id: transaction['id'],
+                                  name: transaction['name'],
+                                  reason: transaction['reason'],
+                                  amount: transaction['amount'],
+                                  imagePath: transaction['imagePath'],
+                                  date: transaction['date']
+                              )
+                          );
+                        },
+                      ).toList(),
                     ),
                   ],
                 );
@@ -145,6 +92,87 @@ class _ExpensesYearTabState extends State<ExpensesYearTab> {
           },
         );
       },
+    );
+  }
+
+  Widget _transactionItem(
+      BuildContext context, {
+        required TransactionProvider provider,
+        required Transaction transaction,
+      }) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          TransactionDetail.routeName,
+          arguments: Transaction(
+            id: transaction.id,
+            name: transaction.name,
+            reason: transaction.reason,
+            amount: transaction.amount,
+            imagePath: transaction.imagePath,
+            date: transaction.date,
+          ),
+        );
+      },
+      child: Container(
+        height: main.transactionHeight,
+        width: double.infinity,
+        margin: const EdgeInsets.only(left: main.horizontalOffset/2, right: main.horizontalOffset/2, bottom: main.verticalOffset),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(main.transactionRadius),
+          color: Theme.of(context).colorScheme.surface.withOpacity(0.7),
+          boxShadow: const [
+            main.boxShadow
+          ],
+        ),
+        constraints: const BoxConstraints(minHeight: main.transactionHeight),
+        clipBehavior: Clip.none,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+          children: [
+            /*
+            CircleAvatar(
+              backgroundColor: Colors.transparent,
+              child: SizedBox(
+                child: Image.file(
+                    File(transaction.imagePath)),
+              ),
+            ),
+             */
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    DateFormat.yMEd(
+                        provider.locale.countryCode)
+                        .format(DateTime.parse(
+                        transaction.date)),
+                    style: TextStyle(
+                        color: Theme.of(context)
+                            .colorScheme
+                            .tertiary,
+                        fontSize: 11),
+                  ),
+                  Expanded(
+                    child: Container(
+                        constraints: const BoxConstraints(maxWidth: 250),
+                        child: Text(transaction.name, style: Theme.of(context).textTheme.titleMedium,)),
+                  ),
+                  Expanded(child: Text(transaction.reason)),
+                  // Text(reason!, style: Theme.of(context).textTheme.bodyMedium,),
+                ],
+              ),
+            ),
+            Text(NumberFormat.currency(locale: provider.locale.countryCode, symbol: "€").format(transaction.amount), style: const TextStyle(color: Colors.green, fontSize: 14.0, fontWeight: FontWeight.w400),)
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/themes/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+// ********** GENERAL **********
+const horizontalOffset = 20.0;
+const verticalOffset = 16.0;
+
+// ********** TRANSACTION ITEM ********
+const transactionHeight = 120.0;
+const boxShadow = BoxShadow(color: Colors.black12, blurRadius: 10, offset: Offset(0, 8));
+const transactionRadius = 23.0;
+
+// ********** HOME SCREEN **************
+const homePrimaryDark = Color(0xff2E2F74);
+const homePrimaryLight = Color(0xff05059B);
+
+const homeHeaderBannerRadius = 36.0;
+const homeHeaderBannerTextColor = Color(0xffD1E2FF);
+
+const lastTransactionContainerBG = Color(0xffD1E2FF);
+const lastTransactionAmountColor = Color(0xff0F9E0D);
+const lastTransactionNameColor = Color(0xff073B8B);
+const lastTransactionRadius = 23.0;
+
+const homeOtherTransactionsRadius = 23.0;
+const homeOtherTransactionsShadow = BoxShadow(
+  color: Colors.black12,
+  blurRadius: 10,
+  offset: Offset(0, 8),
+);
+
+
